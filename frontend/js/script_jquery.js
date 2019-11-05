7ac0bee8792f0cda1920a9c20474511f19d99050
leoalonso99 <FIC@info.local>
2019-11-05 16:48:46 -0200

    Atualização da pagina Home e criação do carrosel

---
+++ frontend/js/script_jquery.js
@@ -0,0 +1,15 @@
+var tab = [
+    "imagens/iconeAdmin/paisagem_carrosel.jpg",
+    "imagens/iconeAdmin/paisagemjpg.jpg",
+    "imagens/iconeAdmin/maxresdefault.jpg"
+]
+
+var i = 0;
+
+$('button').on('click', function() {
+    i++;
+    $('.imagem_carrosel').attr("src", tab[i % 3]);
+    setInterval(function() {
+        $('button').click();
+    }, 2000);
+});
