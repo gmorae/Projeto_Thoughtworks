afbb51ae0c074f916777701288a7578a57d92628
de-carvalho <anacarolina24122706@gmail.com>
2019-11-05 11:51:01 -0200

    atualização

--- backend/Controllers/LoginController.cs
+++ backend/Controllers/LoginController.cs
@@ -36,7 +36,7 @@ namespace backend.Controllers
                     new Claim(JwtRegisteredClaimNames.Email, usuario.Email),
                     new Claim(JwtRegisteredClaimNames.Jti, usuario.IdUsuario.ToString()),
                     new Claim(ClaimTypes.Role, usuario.IdTipoNavigation.NomeTipoUsuario),
-                    new Claim("Role", usuario.IdTipoNavigation.NomeTipoUsuario)
+                    // new Claim("Roles", usuario.IdTipoNavigation.NomeTipoUsuario)
                 };
 
                 var token = new JwtSecurityToken(
