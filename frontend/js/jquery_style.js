b192106bfc2a40050b0526d6be07a00d27d06e40
de-carvalho <anacarolina24122706@gmail.com>
2019-11-06 12:47:52 -0200

    atualização no código da página de todos os eventos

--- frontend/js/jquery_style.js
+++ frontend/js/jquery_style.js
@@ -2,18 +2,18 @@ $(function(){
     $('.botao1').click(function(){
         $('.table2').hide();
         $('.table3').hide();
-        $('.table1').fadeIn(500);
+        $('.table1').fadeIn(250);
     })
 
     $('.botao2').click(function(){
         $('.table3').hide();
         $('.table1').hide();
-        $('.table2').fadeIn(500);
+        $('.table2').fadeIn(250);
     })
 
     $('.botao3').click(function(){
         $('.table2').hide();
         $('.table1').hide();
-        $('.table3').fadeIn(500);
+        $('.table3').fadeIn(250);
     })
 });
