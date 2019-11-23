f5cf5c42ad63ffe7faf236b34d53d85d5d908dd9
gmorae <gmmartins06@gmail.com>
2019-11-23 10:47:23 -0200

    Criando as rotas e as estruturas

--- React/src/index.js
+++ React/src/index.js
@@ -3,10 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//import NotFound from './pages/NotFound';
+import Galeria from './Pages/Galeria/Galeria'
+import Eventos from './Pages/Eventos/Eventos'
+import DetalheEventos from './Pages/DetalheEventos/DetalheEventos'
+import Cadastro from './Pages/Cadastro/Cadastro'
+import Login from './Pages/Login/Login'
+import EsqueceuSenha from './Pages/EsqueceuSenha/EsqueceuSenha'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const Rotas = (
+    <Router>
+        <div>
+            <Switch>
+                {/** Rotas comuns */}
+                <Route path="/" exact component={App} />
+                <Route path="/Galeria" component={Galeria} />
+                <Route path="/Eventos" component={Eventos} />
+                <Route path="/DetalheEvento" component={DetalheEventos} />
+                <Route path="/Cadastro" component={Cadastro} />
+                <Route path="/Login" component={Login} />
+                <Route path="/EsqueceuSenha" component={EsqueceuSenha} />
+
+                {/** Rotas dos usuarios */}
+
+
+                {/** Rotas do administrador */}
+
+
+                {/*<Route component={NotFound} />*/}
+            </Switch>
+        </div>
+    </Router>
+)
+
+ReactDOM.render(Rotas, document.getElementById('root'));
 serviceWorker.unregister();
