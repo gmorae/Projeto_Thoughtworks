b8dacf9b2641f6a94538c21b5784925319db5d43
gmorae <gmmartins06@gmail.com>
2019-11-27 23:22:48 -0200

    Menu ADM

--- React/src/components/cabecalhos/cabecalhoAdmin/CabecalhoAdmin.js
+++ React/src/components/cabecalhos/cabecalhoAdmin/CabecalhoAdmin.js
@@ -1,49 +1,42 @@
 import React, { Component } from 'react'
 import './cabecalhoAdmin.css'
 import '../styleCabecalho.css'
-
 class CabecalhoAdmin extends Component {
+
     render() {
         return (
-            <div className="pos-f-t">
-                <nav className="navbar navbar-light navb">
-                    <button class="btn" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                        <span className="navbar-toggler-icon"></span>
+            <nav class="navbar navbar-dark fixed-top indigo darken-2">
+                <div className="container-fluid">
+                    <button class="navbar-toggler third-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent22"
+                        aria-controls="navbarSupportedContent22" aria-expanded="false" aria-label="Toggle navigation">
+                        <div class="animated-icon3"><span></span><span></span><span></span></div>
                     </button>
-                    <div className="container">
-                        <form className="">
-                            <input type="text" className="form-control" placeholder="Pesquise" />
-                        </form>
-                        <div className="ml-auto">
-                            <button className="navbar-toggler" type="button">
-                                Sair <i className="fas fa-sign-out-alt"></i>
-                            </button>
-                        </div>
-                    </div>
-                </nav>
-                <div className="navbar navbarr navbar-light bg-redeasy">
-
-                    <div className="collapse" id="collapseExample">
-                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                            <li className="nav-item active">
-                                <a className="nav-link ml-4 " href="#">Home <span className="sr-only">(current)</span></a>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                                aria-haspopup="true" aria-expanded="false">Nome User</a>
+                            <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#"><i class="fas fa-power-off"></i> Logout</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent22">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item ">
+                                <a class="nav-link" href="#">Home</a>
                             </li>
-                            <li className="nav-item active">
-                                <a className="nav-link ml-4" href="#">Galeria</a>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
                             </li>
-                            <li className="nav-item active">
-                                <a className="nav-link ml-4" href="#">Eventos</a>
-                            </li>
-                            <li className="nav-item active">
-                                <a className="nav-link ml-4" href="#">Cadastre-se</a>
-                            </li>
-                            <li className="nav-item active">
-                                <a className="nav-link ml-4" href="#">Login</a>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
                             </li>
                         </ul>
                     </div>
                 </div>
-            </div>
+            </nav>
         )
     }
 }
