aa92c706dcd1a13f92d6048f8e5465e73e6c9f87
FIC <FIC@info.local>
2019-11-29 15:39:27 -0300

    Pagina ususario faltando alinhar a foto no card

--- React/src/Pages/User/Home/User.js
+++ React/src/Pages/User/Home/User.js
@@ -1,54 +1,31 @@
 import React from 'react'
 import Navbar from '../../../components/cabecalhos/cabecalhoAdmin/CabecalhoAdmin'
-// import { MDBRow, MDBCol, MDBCard, MDBIcon,} from "mdbreact";
-
-
+import './User.css'
+import Eventos from '../../../img/imagens/iconeAdmin/evento.png'
+import CriarEvento from'../../../img/imagens/iconeAdmin/criareventos.png'
 function Home() {
-    return ( 
-        <div>
-            <Navbar />
-            {/* <section className="text-center my-5">
-      
-      <MDBRow>
-        <MDBCol lg="3" md="6" className="mb-lg-0 mb-4">
-          <MDBCard collection className="z-depth-1-half">
-            <div className="view zoom">
-              <img
-                src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/5.jpg"
-                className="img-fluid"
-                alt=""
-              />
-              <div className="stripe dark">
-                <a href="#!">
-                  <p>
-                    Evento Antigo<MDBIcon icon="angle-right" />
-                  </p>
-                </a>
+  return (
+<div>
+  
+      <Navbar />
+    
+        <div class="mt-5">
+          <div class="card_flex">
+            <a href="#">
+              <div class="card">
+                 <img src={Eventos} alt="" /> 
+                <p>Eventos Antigos</p>
               </div>
-            </div>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol lg="3" md="6" className="mb-lg-0 mb-4">
-          <MDBCard collection className="z-depth-1-half">
-            <div className="view zoom">
-              <img
-                src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/6.jpg"
-                className="img-fluid"
-                alt=""
-              />
-              <div className="stripe dark">
-                <a href="#!">
-                  <p>
-                    Criar Evento<MDBIcon icon="angle-right" />
-                  </p>
-                </a>
+            </a>
+            <a href="#">
+              <div class="card">
+                 <img src={CriarEvento} alt="" />  
+                <p>Criar Eventos</p>
               </div>
-            </div>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </section> */}
+            </a>
+          </div>
         </div>
-    )
-}
+</div>
+        )
+      }
 export default Home
