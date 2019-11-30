1903ca8dfce26d3bc661a54a42f32e8d8b77be3d
gmorae <gmmartins06@gmail.com>
2019-11-30 01:26:05 -0200

    Arrumando as páginas home do admin e usuario

--- React/src/Pages/User/Home/User.js
+++ React/src/Pages/User/Home/User.js
@@ -1,31 +1,13 @@
 import React from 'react'
 import Navbar from '../../../components/cabecalhos/cabecalhoAdmin/CabecalhoAdmin'
-import '../../User/Home/User.css'
-import Eventos from '../../../img/imagens/iconeAdmin/evento.png'
-import CriarEvento from'../../../img/imagens/iconeAdmin/criareventos.png'
+
 function Home() {
   return (
-<div>
-  
+    <div>
+
       <Navbar />
-    
-        <div class="mt-5">
-          <div class="card_flex">
-            <a href="#">
-              <div class="card_user">
-                 <img src={Eventos} alt="" /> 
-                <p>Eventos Antigos</p>
-              </div>
-            </a>
-            <a href="#">
-              <div class="card_user">
-                 <img src={CriarEvento} alt="" />  
-                <p>Criar Eventos</p>
-              </div>
-            </a>
-          </div>
-        </div>
-</div>
-        )
-      }
+
+    </div>
+  )
+}
 export default Home
--- React/src/components/CardAdmin/CardAdmin.js
+++ React/src/components/CardAdmin/CardAdmin.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './CardAdmin.css'
 import EventosAprovar from '../../img/imagens/iconeAdmin/evento.png'
 import Negados from '../../img/imagens/iconeAdmin/negados.png'
@@ -7,53 +7,131 @@ import AdicionarEventos from '../../img/imagens/iconeAdmin/criareventos.png'
 import EventosPassados from '../../img/imagens/iconeAdmin/passados.png'
 import EventosProximos from '../../img/imagens/iconeAdmin/proximos.png'
 
-class CardAdmin extends Component{
-  render(){
-    return(
+import Navbar from '../cabecalhos/cabecalhoAdmin/CabecalhoAdmin'
+
+class CardAdmin extends Component {
+  render() {
+    return (
       <div>
-                <div class="mt-5">
-            <div class="card_flex">
-                <a href="todosOsEventos.html">
-                    <div class="card">
-                      <img src={EventosAprovar} alt=""/>
-                        <p>Eventos para aprovar</p>
+        <Navbar />
+        <div class="container mt-5 py-5">
+          <section class="">
+            <div class="row mt-5">
+              <div class="col-lg-4 col-md-12 mb-4">
+                <div class="card mt-3">
+                  <div class="">
+                    <i class="far fa-money-bill-alt fa-lg primary-color z-depth-2 p-4 ml-3 mt-n3 rounded text-white"></i>
+                    <div class="float-right text-right p-3">
+                      <p class="text-uppercase text-muted mb-1"><small>sales</small></p>
+                      <h4 class="font-weight-bold mb-0">23 000$</h4>
                     </div>
-                </a>
-                <a href="eventosRejeitados.html">
-                    <div class="card">
-                      <img src={Negados} alt=""/>                   
-                        <p>Eventos negados</p>
+                  </div>
+                  <div class="card-body pt-0">
+                    <div class="progress md-progress">
+                      <div class="progress-bar bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0"
+                        aria-valuemax="100"></div>
                     </div>
-                </a>
-                <a href="eventos_aprovados.html">
-                    <div class="card">
-                      <img src={Aprovados} alt=""/>
-                      <p>Eventos aprovados</p>
+                    <p class="card-text">Better than last week (75%)</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-12 mb-4">
+                <div class="card mt-3">
+                  <div class="">
+                    <i class="far fa-money-bill-alt fa-lg primary-color z-depth-2 p-4 ml-3 mt-n3 rounded text-white"></i>
+                    <div class="float-right text-right p-3">
+                      <p class="text-uppercase text-muted mb-1"><small>sales</small></p>
+                      <h4 class="font-weight-bold mb-0">23 000$</h4>
                     </div>
-                </a>
-                <a href="#">
-                    <div class="card">
-                   <img src={AdicionarEventos} alt=""/>
-                        <p>Adicionar evento</p>
+                  </div>
+                  <div class="card-body pt-0">
+                    <div class="progress md-progress">
+                      <div class="progress-bar bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0"
+                        aria-valuemax="100"></div>
                     </div>
-                </a>
-                <a href="">
-                    <div class="card">
-     <img src={EventosPassados} alt=""/>
-                        <p>Eventos passados</p>
+                    <p class="card-text">Better than last week (75%)</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-12 mb-4">
+                <div class="card mt-3">
+                  <div class="">
+                    <i class="far fa-money-bill-alt fa-lg primary-color z-depth-2 p-4 ml-3 mt-n3 rounded text-white"></i>
+                    <div class="float-right text-right p-3">
+                      <p class="text-uppercase text-muted mb-1"><small>sales</small></p>
+                      <h4 class="font-weight-bold mb-0">23 000$</h4>
                     </div>
-                </a>
-                <a href="">
-                    <div class="card">
-                  <img src={EventosProximos} alt=""/>
-                        <p>Próximos eventos</p>
+                  </div>
+                  <div class="card-body pt-0">
+                    <div class="progress md-progress">
+                      <div class="progress-bar bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0"
+                        aria-valuemax="100"></div>
                     </div>
-                </a>
+                    <p class="card-text">Better than last week (75%)</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-12 mb-4">
+                <div class="card mt-3">
+                  <div class="">
+                    <i class="far fa-money-bill-alt fa-lg primary-color z-depth-2 p-4 ml-3 mt-n3 rounded text-white"></i>
+                    <div class="float-right text-right p-3">
+                      <p class="text-uppercase text-muted mb-1"><small>sales</small></p>
+                      <h4 class="font-weight-bold mb-0">23 000$</h4>
+                    </div>
+                  </div>
+                  <div class="card-body pt-0">
+                    <div class="progress md-progress">
+                      <div class="progress-bar bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0"
+                        aria-valuemax="100"></div>
+                    </div>
+                    <p class="card-text">Better than last week (75%)</p>
+                  </div>
+                </div>
+              </div>
+              
+              <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card mt-3">
+                  <div class="">
+                    <i class="fas fa-chart-line fa-lg teal z-depth-2 p-4 ml-3 mt-n3 rounded text-white"></i>
+                    <div class="float-right text-right p-3">
+                      <p class="text-uppercase text-muted mb-1"><small>subscriptions</small></p>
+                      <h4 class="font-weight-bold mb-0">3534</h4>
+                    </div>
+                  </div>
+                  <div class="card-body pt-0">
+                    <div class="progress md-progress">
+                      <div class="progress-bar bg-danger" role="progressbar" aria-valuenow="46" aria-valuemin="0"
+                        aria-valuemax="100"></div>
+                    </div>
+                    <p class="card-text">Worse than last week (46%)</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card mt-3">
+                  <div class="">
+                    <i class="fas fa-chart-pie fa-lg purple z-depth-2 p-4 ml-3 mt-n3 rounded text-white"></i>
+                    <div class="float-right text-right p-3">
+                      <p class="text-uppercase text-muted mb-1"><small>traffic</small></p>
+                      <h4 class="font-weight-bold mb-0">656 234</h4>
+                    </div>
+                  </div>
+                  <div class="card-body pt-0">
+                    <div class="progress md-progress">
+                      <div class="progress-bar bg-success" role="progressbar" aria-valuenow="31" aria-valuemin="0"
+                        aria-valuemax="100"></div>
+                    </div>
+                    <p class="card-text">Better than last week (31%)</p>
+                  </div>
+                </div>
+              </div>
             </div>
+          </section>
         </div>
       </div>
     )
- 
+
   }
 }
 
