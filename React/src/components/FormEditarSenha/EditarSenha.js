859195529b193755d2044f4b47196881d2479cbf
cnnay <carlaalraccn2018@.com>
2019-12-05 16:10:38 -0200

    Pagina Editar senha User

--- React/src/components/FormEditarSenha/EditarSenha.js
+++ React/src/components/FormEditarSenha/EditarSenha.js
@@ -1,14 +1,15 @@
-import EditarSenha from './components/FormEditarSenha/EditarSenha'
+// import EditarSenha from './components/FormEditarSenha/EditarSenha'
 
 import React, { Component } from 'react';
 import './EditarSenha.css'
-import NaVbar from '../cabecalhos/cabecalhoAdmin/CabecalhoAdmin'
+// import NaVbar from '../cabecalhos/cabecalhoAdmin/CabecalhoAdmin'
 
 class FormEditarSenha extends Component {
     render() {
         return (
             <div>
-                <NaVbar />
+                
+                {/* <NaVbar /> */}
                 <div className="div_principal">
                     <div id="editar">
                         <h1>editar senha </h1>
@@ -16,27 +17,28 @@ class FormEditarSenha extends Component {
 
                     {/* <div class="caixa">
                     <i class="fas fa-user-alt"></i>
-                    <input type="text" name="nome" value="" placeholder="seu nome" />
+                    <input type="password" name="nome" value="" placeholder="seu nome" />
                 </div> */}
+                    <form>
+                        <div class="caixa">
+                            <i class="fas fa-key"/>
+                            <input type="password" name="nome" placeholder="senha atual" />
+                        </div>
 
-                    <div class="caixa">
-                        <i class="fas fa-key"></i>
-                        <input type="text" name="nome" value="" placeholder="senha atual" />
-                    </div>
+                        <div class="caixa">
+                            <i class="fas fa-lock"/>
+                            <input type="password" name="nome" placeholder="nova senha" />
+                        </div>
 
-                    <div class="caixa">
-                        <i class="fas fa-lock"></i>
-                        <input type="text" name="nome" value="" placeholder="nova senha" />
-                    </div>
+                        <div class="caixa">
+                            <i class="fas fa-check-circle"/>
+                            <input type="password" name="nome" placeholder="confirme sua nova senha" />
+                        </div>
 
-                    <div class="caixa">
-                        <i class="fas fa-check-circle"></i>
-                        <input type="text" name="nome" value="" placeholder="confirme sua nova senha" />
-                    </div>
-
-                    <div id="botao">
-                        <input type="button" value="registrar" />
-                    </div>
+                        <div id="botao">
+                            <input type="button" value="registrar" />
+                        </div>
+                    </form>
                 </div>
             </div>
         )
